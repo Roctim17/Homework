422182d20542f33bbcdad2ccb0e03ce48d38d1b3
Roctim17 <mdrokibulislamroctim@gmail.com>
2025-09-24 19:18:14 +0800

    donr

--- Homework.py
+++ Homework.py
@@ -1,1 +1,23 @@
 print ("Hello World")
+a = [1,2,3,4,5]
+print(a)
+b = ["Roctim"]
+print(b)
+d = list(range(10))
+print(d)
+e = list(range(100,200,20))
+print(e)
+a.append(6)
+print(a)
+b.insert(2,"Rt17")
+print(b)
+ab = a+b
+print(ab)
+f = [2,3,4,5,2,8]
+f.sort()
+print(f)
+g = ["roctim","roc","Rt","Rt17","Roc"]
+print(g)
+
+
+print("Hello World",a,b,d,e,ab,f,g,e)
