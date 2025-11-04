b79f4f409b67e5fce6854e298aa9a8b2ebeb2478
Roctim17 <mdrokibulislamroctim@gmail.com>
2025-11-04 17:47:04 +0800

    done

--- chap03.py
+++ chap03.py
@@ -24,3 +24,27 @@ import pandas as pd
 pd.DataFrame.quantile(example3_1,q=[0.1,0.25,0.5,0.75,0.9],interpolation='linear')
 
 # page 94
+import pandas as pd
+R=example3_1[''].max()- example3_1[''].min()
+print(':',R)
+
+import numpy as np
+IQR = np.quantile(example3_1['',q=0.25])-np.quantile(example3_1[''],q=0.25)
+print('IQR =',IQR)
+
+#Page 95
+import pandas as pd 
+var = example3_1[''].var(ddof=1)
+print(':',var)
+
+sd=example3_1[''].std()
+print(':'round(sd,2))
+
+#page 96
+import pandas as pd
+import numpy as np
+example2_3 = pd.read_csv('C:\Projects\Python\data\data\chap03\example2_3.CSV', encoding='gbk')
+s_mean = example2_3.mean()
+s_sd = example2_3.std()
+s_cv = s_sd / s_mean
+df = pd.DataFrame
